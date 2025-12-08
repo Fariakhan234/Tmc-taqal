d8f75f35808a9c33919b18bcc0b7c8988f9195f0
Faria234 <shaheerkhan45549@gmail.com>
2025-12-08 14:57:00 +0500

    ...

--- src/app/chat/chat.ts
+++ src/app/chat/chat.ts
@@ -1,10 +1,8 @@
-import { AfterViewChecked, Component, ElementRef, inject, ViewChild } from '@angular/core';
-import { Users } from '../services/users';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterModule } from '@angular/router';
-import { Router } from '@angular/router';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { Router, RouterLink } from '@angular/router';
 
 
 @Component({
